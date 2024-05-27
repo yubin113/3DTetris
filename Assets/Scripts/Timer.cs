eb6664f37c5c07338d5526ae5825c8212baece08
SamSam1326 <166692058+SamSam1326@users.noreply.github.com>
2024-05-27 23:12:05 +0900

    게임 클리어 시 타이머 중단 표기

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -9,6 +9,7 @@ public class Timer : MonoBehaviour
     public TMP_Text timerText; // 타이머를 표시할 UI Text 컴포넌트
     public TMP_Text clearTimeText; // 클리어 시간을 표시할 UI Text 컴포넌트
     private float startTime; // 타이머 시작 시간
+    private bool isGameClear = false; // 게임 클리어 여부를 추적하기 위한 변수
 
     private void Start()
     {
@@ -16,7 +17,7 @@ public class Timer : MonoBehaviour
     }
     void Update()
     {
-        if (GameManager.instance.isSpecialModeActive && !GameManager.instance.IsGameClear() && !GameManager.instance.IsGameOver())
+        if (GameManager.instance.isSpecialModeActive && !isGameClear && !GameManager.instance.IsGameOver())
         {
             float elapsedTime = Time.time - startTime; // 경과 시간 계산
 
@@ -24,8 +25,14 @@ public class Timer : MonoBehaviour
             string elapsedSeconds = (elapsedTime % 60).ToString("f2"); // 소수 둘째자리까지 표시
 
             timerText.text = elapsedMinutes + ":" + elapsedSeconds; // 타이머 텍스트 업데이트
+
+            if (GameManager.instance.IsGameClear())
+            {
+                isGameClear = true; // 게임 클리어 상태로 변경
+                clearTimeText.text = "Clear Time: " + elapsedMinutes + ":" + elapsedSeconds; // 클리어 시간 텍스트 업데이트
+            }
         }
-        else
+        else if (!GameManager.instance.isSpecialModeActive)
         {
             timerText.text = ""; // 특별 모드가 아니라면 타이머 텍스트를 비움
         }
